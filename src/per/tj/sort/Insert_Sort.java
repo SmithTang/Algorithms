1e4f3c9f67c21121c25407a5dc840c48d54a5e50
SmithTang <763714665@qq.com>
2016-08-27 21:55:49 +0800

    enhanced insert sort

--- src/per/tj/sort/Insert_Sort.java
+++ src/per/tj/sort/Insert_Sort.java
@@ -1,21 +1,64 @@
 package per.tj.sort;
 
+/**
+ * @Time 2016-8-27 21:53:56
+ * @version 1.0
+ * @author Coder_T
+ *
+ */
 public class Insert_Sort {
 	
-	public static int[] insertSort(int[] input) {
+	/**
+	 * Type of sort, ascending and descending
+	 * @author Coder_T
+	 *
+	 */
+	enum SortType{ASCENDING, DESCENDING};
+	
+	/**
+	 * @param distorted int[]
+	 * @return new sorted int[]
+	 */
+	public static int[] insertSort(int[] input, SortType type) {
 		System.out.println("Initiating Insert Sort...");
 		
-		for(int i = 1; i < input.length; i++) {
-			int key = input[i];
-			int j = i - 1;
-			while(j >= 0 && input[j] > key) {
-				input[j+1] = input[j];
-				j--;
-			}
-			input[j+1] = key;
-		}
+		int[] output = input.clone();
 		
-		return input;
+		System.out.println("Clone completed, sorting...");
+		int swapCount = 0;
+		if(type == SortType.ASCENDING)
+			for(int i = 1; i < output.length; i++) {
+				int key = output[i];
+				int j = i - 1;
+				while(j >= 0 && output[j] > key) {
+					output[j+1] = output[j];
+					j--;
+					swapCount++;
+					if(swapCount == Integer.MAX_VALUE) {
+						System.out.println("Warning! swap count beyond Integer.Max_Value!");
+						System.exit(0);
+					}
+				}
+				output[j+1] = key;
+			}
+		else if(type == SortType.DESCENDING)
+			for(int i = 1; i < output.length; i++) {
+				int key = output[i];
+				int j = i - 1;
+				while(j >= 0 && output[j] < key) {
+					output[j+1] = output[j];
+					j--;
+					swapCount++;
+					if(swapCount == Integer.MAX_VALUE) {
+						System.out.println("Warning! swap count beyond Integer.Max_Value!");
+						System.exit(0);
+					}
+				}
+				output[j+1] = key;
+			}
+		System.out.println("Sort completed: ");
+		System.out.println("totally swaped " + swapCount + " times.");
+		return output;
 	}
 	
 	public static void main(String[] args) {
@@ -25,7 +68,8 @@ public class Insert_Sort {
 			System.out.print(i + "\t");
 		System.out.println("");
 		
-		int[] output = Insert_Sort.insertSort(input);
+		int[] output = Insert_Sort.insertSort(input, SortType.DESCENDING);
+		
 		System.out.print("Output Integers: ");
 		for(int i:output)
 			System.out.print(i + "\t");
--- src/per/tj/sort/Utils.java
+++ src/per/tj/sort/Utils.java
@@ -2,8 +2,22 @@ package per.tj.sort;
 
 import java.util.Random;
 
+/**
+ * Common Utils:<br>
+ * Math_About: RandomIntegers,<br>
+ * @version 1.0
+ * @author Coder_T
+ * @Time 2016-8-27 21:54:56
+ *
+ */
 public class Utils {
 
+	/**
+	 * 
+	 * @param length
+	 * @param maximum
+	 * @return new int[length] and bounded between 0 ~ maximum
+	 */
 	public static int[] RandomIntegers(int length, int maximum) {
 		
 		int[] randomIntegers = new int[length] ;
